88bf0d8cdd9a5f3328fbf24987257e263b73f0ca
Syed Umair Ul Hassan <64380267+alphadeveloper12@users.noreply.github.com>
2023-09-10 20:10:23 +0500

    Update settings.py

--- running_pops/settings.py
+++ running_pops/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'running_pops.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'db_running_popsy',
+        'USER': 'dev_running_popsy',
+        'PASSWORD' : 'poPSy@12345',
+        'HOST' : 'localhost',
+        'PORT' : '',
     }
 }
 
